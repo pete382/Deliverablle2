a6a6df7dcf033352aec1ea330b94574c92df1ed0
pete382 <ivan382@hotmail.com>
2024-04-25 19:43:37 -0500

    Deliverable2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Security;
 
 
 public class CoinFlip
@@ -10,7 +11,14 @@ public class CoinFlip
         int score = 0;
         int total_score;
 
-        Console.WriteLine("Do you want to try a coin flip game? Type 'Y' for yes and 'N' for no.");
+
+        //Name
+        Console.WriteLine("What's you name?");
+        string name =Console.ReadLine();
+
+        //Coin Flip
+       
+        Console.WriteLine("Hello, "+name + ". Do you want to try a coin flip game? Type 'Y' for yes and 'N' for no.");
         string Answer = Console.ReadLine();
         if (Answer == "N") { Console.WriteLine("You got to be kidding. Are you chicken?"); }
         else
@@ -20,20 +28,24 @@ public class CoinFlip
 
                 {
 
-                    Console.WriteLine("For heads type 'H'. For tails type T.");
+                    Console.WriteLine(name +" ,for heads type 'H'. For tails type T.");
                     string Guess = Console.ReadLine();
-                    int Flip = rnd.Next(0, 1);
+                    int Flip = rnd.Next(0, 2);
                     string Flipstring = Flip.ToString();
-
-                    if (Flipstring == "0" && Guess == "H") { Console.WriteLine(total_score = score + 1); }
-                    else if (Flipstring == "1" && Guess == "T") { Console.WriteLine(total_score = score + 1); }
-                    else { Console.WriteLine("0"); }
+                    if (Flipstring == "0" && Guess == "H" || Flipstring == "1" && Guess == "T") { score = score +1; }
+                       else {score = score + 0;}
                     Console.WriteLine(Flipstring);
-                    Console.WriteLine(Guess);
-                    Console.WriteLine(score);
+                  
                 }
+                int total = score;
+                             
                 i++;
-                
+
+                //Total Score 
+
+                if (i == 5)
+                { Console.WriteLine("Thank you, " + name + ". You have a score of:" + total + "."); } 
             }
     }
-} //| Flipstring == "1" && Guess == "T"
+    
+}
--- obj/Debug/net8.0/Deliverable2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Deliverable2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deliverable2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f45ee25a60fafb6130bf09acc0507832ad21f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deliverable2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deliverable2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
